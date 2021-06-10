a0d649ef24dcba35cbac4ef46516901c6c922cc2
Octavio <tudaeva@gmail.com>
2021-06-10 11:38:52 -0700

    walked through some examples

--- exer.js
+++ exer.js
@@ -4,3 +4,24 @@
 
 // Create a function that takes his current age and returns the given age 20 (or 21) years, with number base in the format specified in the below examples.
 
+// Find the base
+// 21 = 65
+// 65 - 1 = 64
+// 64 / 2 = 32
+//  => 65 is 21 in base 32
+
+// 20 = 65
+// 65 - 0 = 65
+// 65 / 2 = 32 1/2
+//  => 65 is 20 in base 32 1/2
+
+// 21 = 22
+// 22 - 1 = 21
+// 21 / 2 = 10 1/2
+//
+
+// 20 = 22
+// 22 - 0 = 22
+// 22 / 2 = 11
+//  => 22 is 20 in base 11
+
