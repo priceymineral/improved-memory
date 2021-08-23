6abdc9fe7262e0706a5221b73e824f86ab26aa3f
Octavio <tudaeva@gmail.com>
2021-08-23 16:40:15 -0700

    added and solved averagePair

--- exer.js
+++ exer.js
@@ -1200,3 +1200,23 @@ function areThereDuplicates() {
 // areThereDuplicates(3, 2, 1, 4);
 // areThereDuplicates(10, 2, 1);
 // areThereDuplicates('a', 'b', 'c', 'c', 'b');
+
+// Determine if there is a pair that adds up to target average
+function averagePair(arr, num) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    let avg = arr[left] + arr[right] / 2;
+    if (avg === num) {
+      return true;
+    } else if (avg < num) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return false;
+}
+averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8);
