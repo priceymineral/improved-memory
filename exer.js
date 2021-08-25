dc8a015fe0ec666c9ce997413cf7cd27922e0043
Octavio <tudaeva@gmail.com>
2021-08-25 11:09:33 -0700

    added solution to minSubArrayLen O(n) time/O(1) space

--- exer.js
+++ exer.js
@@ -1327,7 +1327,35 @@ function minSubArrayLen(arr, num) {
   console.log('minLen:', minLen);
   // return minLen;
 }
+// WORKING SOLUTION
+function minSubArrayLen(nums, sum) {
+  let total = 0;
+  let start = 0;
+  let end = 0;
+  let minLen = Infinity;
 
+  while (start < nums.length) {
+    // if current window doesn't add up to the given sum then
+    // move the window to right
+    if (total < sum && end < nums.length) {
+      total += nums[end];
+      end++;
+    }
+    // if current window adds up to at least the sum given then
+    // we can shrink the window
+    else if (total >= sum) {
+      minLen = Math.min(minLen, end - start);
+      total -= nums[start];
+      start++;
+    }
+    // current total less than required total but we reach the end, need this or else we'll be in an infinite loop
+    else {
+      break;
+    }
+  }
+
+  return minLen === Infinity ? 0 : minLen;
+}
 // minSubArrayLen([2,3,1,2,4,3], 7) // 2
 minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 55) // 5
   // minSubArrayLen([1,4,16,22,5,7,8,9,10], 39) // 3
