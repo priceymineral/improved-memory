00541f608dde4070e42cf5a19482b824835bc601
Octavio <tudaeva@gmail.com>
2021-08-30 16:23:25 -0700

    added and solved selectionSort

--- exer.js
+++ exer.js
@@ -1419,3 +1419,36 @@ function bubbleSort(arr, n = 1) {
 }
 
 // bubbleSort([4,5,3,6,1,7,3,1,5,3]);
+
+function selectionSort(arr) {
+  // input is an array of numbers
+  // output is the input array sorted
+  // edge - arr is empty
+  //
+  // constraints - do not use native sorting methods
+
+  // High Level - find the smallest number in the array and swap with the beginning
+
+  for (let i = 0; i < arr.length; i++) {
+    let idx = i;
+    let min = arr[i];
+    for (let j = i; j < arr.length; j++) {
+      if (arr[j] < min) {
+        min = arr[j]
+        idx = j;
+      }
+    }
+    if (idx !== i) {
+      console.log('swap')
+      // [arr[i], arr[idx]] = [arr[idx], arr[i]];
+      var temp = arr[i];
+      arr[i] = arr[idx];
+      arr[idx] = temp;
+    }
+  }
+
+  return arr;
+}
+
+// selectionSort([4,5,3]);
+// selectionSort([4,5,3,6,1,7,3,1,5,3]);
