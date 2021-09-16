27e55be5d1f2afc52f2cac24baa9fa5c3fd132f1
Octavio <tudaeva@gmail.com>
2021-09-16 16:31:29 -0700

    added pop() method to stack.js

--- exercises/structures/stack.js
+++ exercises/structures/stack.js
@@ -46,11 +46,45 @@ class Stack {
 
   pop() {
     // pop from the front of the stack
+    // input - none
+    // output - popped node
+    // edge - stack is empty, return null
+    //      - if stack only one node long, reassign first and last (null)
+    // constraints - none stated
+    // high level - remove the first node and return it, make necessary mods to the stack (ie, first, last size)
+
+    // if stack s empty, return null
+    if (!this.size) return null;
+    // save the original first node to a variable
+    let popped = this.first;
+    // if stack is 1 node long
+    if (this.size === 1) {
+      // reassign first to null
+      this.first = null;
+      // reassign last to null
+      this.last = null;
+      // else
+    } else {
+      // reassign first to the second node
+        this.first = this.first.next;
+    }
+
+    // decrement size
+    this.size--;
+    // return original first node
+    return popped;
+
   }
 }
 
 let myStack = new Stack();
 myStack.push(1);
+// console.log(myStack.push(1));
 myStack.push(2);
 myStack.push(3);
+console.log(myStack.pop());
+// console.log('what is happening here')
+// myStack.pop();
 console.log(myStack);
+
+
