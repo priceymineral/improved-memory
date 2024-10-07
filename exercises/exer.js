3d008aeddc5a6a681f9d023ac9600b54068271f3
Octavio Sifuentes <tudaeva@gmail.com>
2024-10-07 15:47:40 -0700

    largest local values in matrix

--- exercises/exer.js
+++ exercises/exer.js
@@ -1458,3 +1458,45 @@ var countSubIslands = function (grid1, grid2) {
   console.log(visited);
   console.log(islands);
 };
+
+// You are given an n x n integer matrix grid.
+
+// Generate an integer matrix maxLocal of size (n - 2) x (n - 2) such that:
+
+// maxLocal[i][j] is equal to the largest value of the 3 x 3 matrix in grid centered around row i + 1 and column j + 1.
+// In other words, we want to find the largest value in every contiguous 3 x 3 matrix in grid.
+// Input: grid = [[9,9,8,1],[5,6,2,6],[8,2,6,4],[6,2,2,2]]
+// Output: [[9,9],[8,6]]
+// Explanation: The diagram above shows the original matrix and the generated matrix.
+// Notice that each value in the generated matrix corresponds to the largest value of a contiguous 3 x 3 matrix in grid.
+
+var largestLocal = function (grid) {
+  const matrix = [];
+  let arr = [];
+
+  let checker = (m, n) => {
+    let max = 0;
+
+    for (i = m - 1; i <= m + 1; i++) {
+      for (j = n - 1; j <= n + 1; j++) {
+        if (grid[i][j] > max) {
+          max = grid[i][j];
+        }
+      }
+    }
+
+    arr.push(max);
+  };
+
+  for (let i = 1; i <= grid.length - 2; i++) {
+    for (let j = 1; j <= grid.length - 2; j++) {
+      checker(i, j);
+    }
+  }
+
+  for (let i = 0; i < arr.length; i += grid.length - 2) {
+    matrix.push(arr.slice(i, i + grid.length - 2));
+  }
+
+  return matrix;
+};
