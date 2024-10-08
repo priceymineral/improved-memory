545a7c800a67a4e88e8e8c071929c749c3ae8e57
Octavio Sifuentes <tudaeva@gmail.com>
2024-10-08 15:37:01 -0700

    reverse integer

--- exercises/exer.js
+++ exercises/exer.js
@@ -1500,3 +1500,46 @@ var largestLocal = function (grid) {
 
   return matrix;
 };
+
+// Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+// Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+// Example 1:
+
+// Input: x = 123
+// Output: 321
+// Example 2:
+
+// Input: x = -123
+// Output: -321
+// Example 3:
+
+// Input: x = 120
+// Output: 21
+
+var reverse = function(x) {
+  let stringX = x.toString()
+  if (stringX.length === 1) return x;
+
+  let reverseString = (str) => {
+      return str.split("").reverse().join("");
+  }
+
+  if (x < 1) {
+      if (stringX.length === 2) {
+       return x;   
+      } else {
+          let output = "-";
+          stringX = stringX.slice(1);
+          let reversed = reverseString(stringX);
+          output += reversed;
+          let numOutput = Number(output);
+          if (numOutput < -(2**31) || numOutput > (2**31)-1) return 0;
+          return numOutput;
+      };
+  }
+
+  let output = Number(reverseString(stringX));
+  if ( output < -(2**31) || output > (2**31)-1) return 0;
+  return output;
+};
